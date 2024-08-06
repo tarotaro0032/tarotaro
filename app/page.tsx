97a4ecf88f3aa08c5ce884fe315adfd3bac82063
Vsevolod Kaliakin <jakeltree@yandex.ru>
2024-08-06 21:12:18 +0300

    added image

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import taroCards from '../public/taroCards.png'
 interface Props {
   name: string;
 }
@@ -9,7 +10,7 @@ const Button = ({ name }: Props) => {
 export default function Home() {
   return (
     <main className="bg-white flex min-h-screen flex-col items-center justify-between p-24 text-2xl font-sans opacity-80">
-      <img src="https://downloader.disk.yandex.ru/preview/c6153d6dcbe22d86e7e8c3203d64a9f991648bd8267025b1afaeac7556cfd9e5/66b299dd/ow36_I8qul9IN1FP8PZqMCslfoK4rtt-Vh7xyozsDI0h_Ya10424uurBud98LeQpDkdlZ4hzdThyr15lQ9IScw%3D%3D?uid=0&filename=hello.png&disposition=inline&hash=&limit=0&content_type=image%2Fpng&owner_uid=0&tknv=v2&size=2048x2048"/>
+      <Image width={200} height={120} alt="taro cards image" src={taroCards}/>
       <h1 className="text-blue-500 font-bold text-center drop-shadow-xl leading-6">Гадание таро<br/> - <br/> расклады на картах онлайн</h1>
       <div className="flex flex-col gap-1.5">
          <Button name="Карта дня"/>
